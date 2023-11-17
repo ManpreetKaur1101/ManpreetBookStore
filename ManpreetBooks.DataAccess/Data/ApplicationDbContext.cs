a118ff2327c396d3d84b140beb85a75182085835
W0812152 <W0812152@ADL-A1201I-06>
2023-11-17 16:43:05 -0500

    i started my assignment 2 part 3

--- ManpreetBooks.DataAccess/Data/ApplicationDbContext.cs
+++ ManpreetBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace ManpreetBookStore.DataAccess.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> CoverType { get; set; }
     }
 }
